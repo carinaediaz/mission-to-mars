1b910da6d3ffdf5522211099808e666fbc1da115
carinaediaz <carinaediaz96@gmail.com>
2022-06-26 11:53:23 -0500

    final scraping.py editing

--- scraping.py
+++ scraping.py
@@ -1,13 +1,30 @@
 # Import Splinter and BeautifulSoup
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
-from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import datetime as dt
+from webdriver_manager.chrome import ChromeDriverManager
 
 
-# Set up Splinter
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+def scrape_all():
+    # Initiate headless driver for deployment
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+
+    news_title, news_paragraph = mars_news(browser)
+
+    # Run all scraping functions and store results in dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+    # Stop webdriver and return data
+    browser.quit()
+
+    return data
 
 
 # Scrape Mars News
@@ -31,6 +48,7 @@ def mars_news(browser):
         news_title = slide_elem.find('div', class_='content_title').get_text()
         # Use the parent element to find the paragraph text
         news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+    
     except AttributeError:
         return None, None
 
@@ -73,15 +91,18 @@ def mars_facts():
     try: 
         # Use 'read_html" to scrape the facts table into a dataframe
         df = pd.read_html('https://galaxyfacts-mars.com')[0]
+    
     except BaseException:
         return None
 
     # Assign columns and set index of dataframe
-    df.columns=['description', 'Mars', 'Earth']
-    df.set_index('description', inplace=True)
+    df.columns=['Description', 'Mars', 'Earth']
+    df.set_index('Description', inplace=True)
 
     # Convert dataframe into HTML format, add bootstrap
-    return df.to_html()
+    return df.to_html(classes="table table-striped")
 
+if __name__ == "__main__":
 
-browser.quit()
+    # If running as script, print scraped data
+    print(scrape_all())
