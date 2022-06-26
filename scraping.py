e6eb77f94deef5b3205b6baed06a837c6fa3506a
carinaediaz <carinaediaz96@gmail.com>
2022-06-26 11:37:48 -0500

    adding more functions and error handling

--- scraping.py
+++ scraping.py
@@ -10,7 +10,7 @@ executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
 
 
-#Scrape Mars News
+# Scrape Mars News
 def mars_news(browser):
 
     # Visit the mars nasa news site
@@ -37,45 +37,51 @@ def mars_news(browser):
     return news_title, news_p
 
 
-### JPL Space Images Featured Image
+# Scrape Featured Image
+def featured_image(browser):
+
+    # Visit URL
+    url = 'https://spaceimages-mars.com'
+    browser.visit(url)
+
+    # Find and click the full image button
+    full_image_elem = browser.find_by_tag('button')[1]
+    full_image_elem.click()
+
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
+    
+    # Add try/except for error handling
+    try:
+        # Find the relative image url
+        img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+
+    except AttributeError:
+        return None
+
+    # Use the base URL to create an absolute URL
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+
+    return img_url
 
 
-# Visit URL
-url = 'https://spaceimages-mars.com'
-browser.visit(url)
+# Scrape Mars Facts
+def mars_facts():
 
+    # Add try/except for error handling
+    try: 
+        # Use 'read_html" to scrape the facts table into a dataframe
+        df = pd.read_html('https://galaxyfacts-mars.com')[0]
+    except BaseException:
+        return None
 
-# Find and click the full image button
-full_image_elem = browser.find_by_tag('button')[1]
-full_image_elem.click()
+    # Assign columns and set index of dataframe
+    df.columns=['description', 'Mars', 'Earth']
+    df.set_index('description', inplace=True)
 
-
-# Parse the resulting html with soup
-html = browser.html
-img_soup = soup(html, 'html.parser')
-
-
-# Find the relative image url
-img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
-img_url_rel
-
-
-# Use the base URL to create an absolute URL
-img_url = f'https://spaceimages-mars.com/{img_url_rel}'
-img_url
-
-
-### Mars Facts
-
-
-df = pd.read_html('https://galaxyfacts-mars.com')[0]
-df.columns=['description', 'Mars', 'Earth']
-df.set_index('description', inplace=True)
-df
-
-
-df.to_html()
+    # Convert dataframe into HTML format, add bootstrap
+    return df.to_html()
 
 
 browser.quit()
-
